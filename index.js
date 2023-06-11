3d681a279af78786dffbdda065bc2f9dfa358dd8
Yoshihiro Tanaka <contact@cordea.jp>
2023-06-11 15:11:52 +0900

    support the drag gesture

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+import {DragGesture} from "@use-gesture/vanilla";
+
+const params = {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0
+}
+
+const img = new Image()
+
 function draw() {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-    const img = new Image()
-    img.onload = () => imageOnLoad(img, ctx)
+    img.onload = imageOnLoad
     img.src = 'assets/image.jpg'
 }
 
-function imageOnLoad(img, ctx) {
-    ctx.drawImage(img, 0, 0)
+function imageOnLoad() {
+    const canvas = document.getElementById("canvas");
+    const aspectRatio = img.width / img.height
+    const width = canvas.height * aspectRatio
+    const height = canvas.height
+    params.width = width
+    params.height = height
+
+    new DragGesture(canvas, (state) => {
+        if (!state.active) {
+            return
+        }
+        params.x += state.delta[0]
+        params.y += state.delta[1]
+    })
+
+    animate()
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.drawImage(
+        img,
+        params.x,
+        params.y,
+        params.width,
+        params.height
+    )
 }
 
 window.onload = draw
